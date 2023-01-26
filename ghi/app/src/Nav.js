eccb637db6b795fa51276fc5779226c61178530c
Christian Rodriguez <105233007+ChrisAlexRods@users.noreply.github.com>
2023-01-26 15:23:21 -0500

    NavBar WIP

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -1,53 +1,190 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+
 
 function Nav() {
+  const [open, setOpen] = useState(false);
+  const [open2, setOpen2] = useState(false);
+  const [open3, setOpen3] = useState(false);
+  const [open4, setOpen4] = useState(false);
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-success nav-custom">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">CarCar</NavLink>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <NavLink className="navbar-brand" to="/">
+          CarCar
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/ManufacturerForm">Manufacturer Form</NavLink>
-            </li>
+            <div className="dropdown ml-3">
+              <button className="btn btn-secondary" onClick={() => setOpen(!open)}>
+                Manufacturer
+              </button>
+              <div
+                class={`dropdown-menu ${open ? "show" : ""}`}
+                aria-labelledby="dropdownMenuButton"
+              >
+                <li className="nav-item">
+                  <NavLink
+                    className="dropdown-item"
+                    aria-current="page"
+                    to="/ManufacturerForm"
+                  >
+                    Manufacturer Form
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    className="dropdown-item"
+                    aria-current="page"
+                    to="/ManufacturerList"
+                  >
+                    Manufacturer List
+                  </NavLink>
+                </li>
+              </div>
+            </div>
+
+            <div className="dropdown ml-3">
+              <button
+                className="btn btn-secondary"
+                onClick={() => setOpen2(!open2)}
+              >
+                Models
+              </button>
+              <div
+                className={`dropdown-menu ${open2 ? "show" : ""}`}
+                aria-labelledby="dropdownMenuButton"
+              >
+                <li className="nav-item">
+                  <NavLink
+                    className="dropdown-item"
+                    aria-current="page"
+                    to="/ModelForm"
+                  >
+                    Model Form
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    className="dropdown-item"
+                    aria-current="page"
+                    to="/ModelList"
+                  >
+                    Model List
+                  </NavLink>
+                </li>
+              </div>
+            </div>
+
+            <div className="dropdown ml-3">
+              <button
+                className="btn btn-secondary"
+                onClick={() => setOpen3(!open3)}
+              >
+                Automobile
+              </button>
+              <div
+                className={`dropdown-menu ${open3 ? "show" : ""}`}
+                aria-labelledby="dropdownMenuButton"
+              >
+                <li className="nav-item">
+                  <NavLink
+                    className="dropdown-item"
+                    aria-current="page"
+                    to="/AutomobileForm"
+                  >
+                    Automobile Form
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    className="dropdown-item"
+                    aria-current="page"
+                    to="/AutomobileList"
+                  >
+                    Automobile List
+                  </NavLink>
+                </li>
+              </div>
+            </div>
+
+            <div className="dropdown ml-3">
+              <button
+                className="btn btn-secondary"
+                onClick={() => setOpen4(!open4)}
+              >
+                Sales
+              </button>
+              <div
+                className={`dropdown-menu ${open4 ? "show" : ""}`}
+                aria-labelledby="dropdownMenuButton"
+              >
+                <li className="nav-item">
+                  <NavLink
+                    className="dropdown-item"
+                    aria-current="page"
+                    to="/SalesPersonForm"
+                  >
+                    Sales Person Form
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    className="dropdown-item"
+                    aria-current="page"
+                    to="/SalesRecordForm"
+                  >
+                    Sales Record Form
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    className="dropdown-item"
+                    aria-current="page"
+                    to="/CustomerForm"
+                  >
+                    Customer Form
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink
+                    className="dropdown-item"
+                    aria-current="page"
+                    to="/SalesRecordList"
+                  >
+                    Sales Record List
+                  </NavLink>
+                </li>
+              </div>
+            </div>
+
             <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/ManufacturerList">Manufacturer List</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/ModelForm">Model Form</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/ModelList">Model List</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/AutomobileForm">Automobile Form</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/AutomobilesList">Automobile List</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/SalesPersonForm">Sales Person Form</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/CustomerForm">Customer Form</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/SalesRecordForm">Sales Record Form</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/TechnicianForm">Technician Form</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link active" aria-current="page" to="/SalesRecordList">Sales Record List</NavLink>
+              <NavLink
+                className="nav-link active"
+                aria-current="page"
+                to="/TechnicianForm"
+              >
+                Technician Form
+              </NavLink>
             </li>
           </ul>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
 export default Nav;
